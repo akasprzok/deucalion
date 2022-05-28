f88bcb3bb0385a4d9b1eefdfd105c4bd6caa73e5
Andreas Kasprzok <akasprzok@gmail.com>
2022-05-28 13:18:32 -0700

    Mix Project and Hex stuff for eventual release

--- mix.exs
+++ mix.exs
@@ -1,13 +1,22 @@
 defmodule Deucalion.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/akasprzok/deucalion"
+
   def project do
     [
       app: :deucalion,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Hex-specific
+      description: description(),
+      package: package(),
+      source_url: @url,
+      docs: docs()
     ]
   end
 
@@ -24,7 +33,29 @@ defmodule Deucalion.MixProject do
       {:nimble_parsec, "~> 1.0"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: :dev, runtime: false},
-      {:git_hooks, "~> 0.7", only: :dev, runtime: false}
+      {:git_hooks, "~> 0.7", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
+    ]
+  end
+
+  defp description do
+    """
+    Deucalion is a Prometheus parser.
+    """
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => @url},
+      maintainers: ["Andreas Kasprzok"]
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Deucalion",
+      extras: ["README.md"]
     ]
   end
 end
